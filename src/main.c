fe08bd034262f7135f9c4b58d85174e5c4506050
Henry Till <henrytill@gmail.com>
2022-03-02 13:06:30 -0800

    refine error handling

--- src/main.c
+++ src/main.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 #include "SDL.h"
-
-const int      SCREEN_WIDTH  = 640;
-const int      SCREEN_HEIGHT = 480;
-const uint32_t DELAY         = 2000;
+#include "util.h"
 
 int
 main(int argc, char *argv[])
 {
-	int          ret     = 1;
-	SDL_Window  *window  = NULL;
-	SDL_Surface *surface = NULL;
-	uint32_t     fill_color;
+	const int      SCREEN_WIDTH  = 640;
+	const int      SCREEN_HEIGHT = 480;
+	const uint32_t DELAY         = 2000;
+	int            ret           = 1;
+	SDL_Window    *window        = NULL;
+	SDL_Surface   *surface       = NULL;
+	uint32_t       fill_color    = 0;
 
 	(void)argc;
 	(void)argv;
 
 	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
-		fprintf(stderr, "SDL_Init: %s\n", SDL_GetError());
+		util_print_sdl_error();
 		goto cleanup;
 	}
 
@@ -29,26 +29,25 @@ main(int argc, char *argv[])
 	                          SCREEN_HEIGHT,
 	                          SDL_WINDOW_SHOWN);
 	if (window == NULL) {
-		fprintf(stderr, "SDL_CreateWindow: %s\n", SDL_GetError());
+		util_print_sdl_error();
 		goto cleanup;
 	}
 
 	surface = SDL_GetWindowSurface(window);
 	if (surface == NULL) {
-		fprintf(stderr, "SDL_GetWindowSurface: %s\n", SDL_GetError());
+		util_print_sdl_error();
 		goto cleanup;
 	}
 
 	fill_color = SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF);
 
 	if (SDL_FillRect(surface, NULL, fill_color) != 0) {
-		fprintf(stderr, "SDL_FillRect: %s\n", SDL_GetError());
+		util_print_sdl_error();
 		goto cleanup;
 	}
 
 	if (SDL_UpdateWindowSurface(window) != 0) {
-		fprintf(
-		    stderr, "SDL_UpdateWindowSurface: %s\n", SDL_GetError());
+		util_print_sdl_error();
 		goto cleanup;
 	};
 
