88851238cb2671d4c27d773b6585555319e823dd
Henry Till <henrytill@gmail.com>
2022-03-03 19:59:53 -0800

    add util_buffer_t and operations on it

---
+++ test/util-buffer-test.c
@@ -0,0 +1,86 @@
+#include <assert.h>
+
+#if defined(_MSC_VER) && defined(_DEBUG)
+#include <crtdbg.h>
+#endif
+
+#include "util.h"
+
+/* Some helper macros */
+
+#define CHECKED_INIT(buff, size) assert(util_buffer_init(buff, size) == 0)
+
+#define CHECKED_DEINIT(buff) assert(util_buffer_deinit(buff) == 0)
+
+#define CHECKED_PUSH(buff, item) assert(util_buffer_push(buff, item) == 0)
+
+#define CHECKED_READ(buff, index, expected)                                    \
+	do {                                                                   \
+		char out;                                                      \
+		assert(util_buffer_read(buff, index, &out) == 0);              \
+		assert(out == (expected));                                     \
+	} while (0)
+
+#define CHECKED_SET(buff, index, item)                                         \
+	assert(util_buffer_set(buff, index, item) == 0)
+
+int
+main(int argc, char *argv[])
+{
+	util_buffer_t buffer;
+
+	(void)argc;
+	(void)argv;
+
+#if defined(_MSC_VER) && defined(_DEBUG)
+	_CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_DEBUG);
+#endif
+
+	CHECKED_INIT(&buffer, 3);
+
+	assert(buffer.size == 3);
+	assert(buffer.curr == 0);
+	assert(*(buffer.data + 0) == 0);
+	assert(*(buffer.data + 1) == 0);
+	assert(*(buffer.data + 2) == 0);
+
+	CHECKED_READ(&buffer, 0, 0);
+	CHECKED_READ(&buffer, 1, 0);
+	CHECKED_READ(&buffer, 2, 0);
+
+	CHECKED_PUSH(&buffer, 0);
+	CHECKED_PUSH(&buffer, 1);
+	CHECKED_PUSH(&buffer, 2);
+
+	assert(buffer.curr == 3);
+
+	CHECKED_READ(&buffer, 0, 0);
+	CHECKED_READ(&buffer, 1, 1);
+	CHECKED_READ(&buffer, 2, 2);
+
+	CHECKED_PUSH(&buffer, 3);
+
+	assert(buffer.size == 6);
+	assert(buffer.curr == 4);
+	assert(buffer.data != NULL);
+
+	CHECKED_READ(&buffer, 0, 0);
+	CHECKED_READ(&buffer, 1, 1);
+	CHECKED_READ(&buffer, 2, 2);
+	CHECKED_READ(&buffer, 3, 3);
+
+	CHECKED_SET(&buffer, 3, 42);
+
+	CHECKED_READ(&buffer, 0, 0);
+	CHECKED_READ(&buffer, 1, 1);
+	CHECKED_READ(&buffer, 2, 2);
+	CHECKED_READ(&buffer, 3, 42);
+
+	CHECKED_DEINIT(&buffer);
+
+	assert(buffer.size == 0);
+	assert(buffer.curr == 0);
+	assert(buffer.data == NULL);
+
+	return 0;
+}
