196a5f176ec9437575aeb30afd57ae0f5aa8c713
Henry Till <henrytill@gmail.com>
2022-03-06 13:39:15 -0800

    remove usage of assert in tests

---
+++ test/test.h
@@ -0,0 +1,14 @@
+#ifndef SDL_BITS_TEST_H
+#define SDL_BITS_TEST_H
+
+#include <stdio.h>
+
+#define TEST(expr)                                                             \
+	do {                                                                   \
+		if (!(expr)) {                                                 \
+			fprintf(stderr, "FAIL: %s:%d", __FILE__, __LINE__);    \
+			return 1;                                              \
+		}                                                              \
+	} while (0)
+
+#endif /* SDL_BITS_TEST_H */
