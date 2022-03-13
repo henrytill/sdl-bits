78fd39bea88cd44f0e07a5b287a49fd219b9f1bb
Henry Till <henrytill@gmail.com>
2022-03-13 14:22:29 -0700

    font_loader: render multiple characters

--- src/font_loader.c
+++ src/font_loader.c
@@ -24,14 +24,17 @@
 
 enum { WIDTH = 10, HEIGHT = 20 };
 
-static char *const FONT_FILE = "./assets/ucs-fonts/10x20.bdf";
-static const char  CHAR_CODE = 'Q';
+static char *const FONT_FILE  = "./assets/ucs-fonts/10x20.bdf";
+static char *const CHAR_CODES = "ABCDEFGHIJ";
 
 static unsigned char
 get_bit(unsigned char x, unsigned int p);
 
 static int
 render_char(FT_GlyphSlot slot, unsigned char **target, size_t offset);
+
+static int
+alloc_image(unsigned char ***image, size_t height, size_t width);
 
 // p = 0 is MSB
 static unsigned char
@@ -42,6 +45,26 @@ get_bit(unsigned char x, unsigned int p)
 		return 0;
 	}
 	return (x >> (CHAR_BIT + ~p)) & 1;
+}
+
+static int
+alloc_image(unsigned char ***image, size_t height, size_t width)
+{
+	size_t i;
+
+	*image = malloc(height * sizeof(char *));
+	if (*image == NULL) {
+		return 1;
+	}
+
+	for (i = 0; i < height; i++) {
+		*(*image + i) = malloc(width * sizeof(char));
+		if (*(*image + i) == NULL) {
+			return 1;
+		}
+	}
+
+	return 0;
 }
 
 static int
@@ -69,7 +92,7 @@ render_char(FT_GlyphSlot slot, unsigned char **target, size_t offset)
 
 				x = j + (i * CHAR_BIT);
 				if (x < width) {
-					*(*(target + y) + x) = datum;
+					*(*(target + y) + x + (offset * width)) = datum;
 				}
 			}
 		}
@@ -86,10 +109,10 @@ main(int argc, char *argv[])
 	FT_Face         face    = NULL;
 	FT_GlyphSlot    slot    = NULL;
 	unsigned char **image   = NULL;
-	unsigned int    rows;
-	unsigned int    width;
-	unsigned char   datum;
 	int             error;
+	size_t          char_codes_len;
+	size_t          image_width;
+	size_t          image_height;
 
 	(void)argc;
 	(void)argv;
@@ -98,10 +121,13 @@ main(int argc, char *argv[])
 
 	assert(CHAR_BIT == EXPECTED_CHAR_BIT);
 
-	size_t i;
-	image = malloc(HEIGHT * sizeof(char *));
-	for (i = 0; i < HEIGHT; i++) {
-		*(image + i) = malloc(WIDTH * sizeof(char));
+	char_codes_len = strlen(CHAR_CODES);
+
+	image_width  = WIDTH * char_codes_len;
+	image_height = HEIGHT;
+	error        = alloc_image(&image, image_height, image_width);
+	if (error != 0) {
+		goto cleanup;
 	}
 
 	error = FT_Init_FreeType(&library);
@@ -120,47 +146,45 @@ main(int argc, char *argv[])
 		goto cleanup;
 	}
 
-	slot = face->glyph;
-
 	error = FT_Set_Pixel_Sizes(face, WIDTH, HEIGHT);
 	if (error != 0) {
 		print_error(error);
 		goto cleanup;
 	}
 
-	error = FT_Load_Char(face, CHAR_CODE, FT_LOAD_NO_SCALE | FT_LOAD_MONOCHROME);
-	if (error != 0) {
-		print_error(error);
-		goto cleanup;
+	size_t i;
+	for (i = 0; i < char_codes_len; i++) {
+		error = FT_Load_Char(face, CHAR_CODES[i], FT_LOAD_NO_SCALE | FT_LOAD_MONOCHROME);
+		if (error != 0) {
+			print_error(error);
+			goto cleanup;
+		}
+
+		slot = face->glyph;
+
+		error = FT_Render_Glyph(slot, FT_RENDER_MODE_MONO);
+		if (error != 0) {
+			print_error(error);
+			goto cleanup;
+		}
+
+		if (slot->format != FT_GLYPH_FORMAT_BITMAP) {
+			fprintf(stderr, "format was not FL_GLYPH_FORMAT_BITMAP");
+			goto cleanup;
+		}
+
+		if (slot->bitmap.pixel_mode != FT_PIXEL_MODE_MONO) {
+			fprintf(stderr, "pixel_mode was not FL_PIXEL_MODE_MONO");
+		}
+
+		render_char(slot, image, i);
 	}
-
-	error = FT_Render_Glyph(face->glyph, FT_RENDER_MODE_MONO);
-	if (error != 0) {
-		print_error(error);
-		goto cleanup;
-	}
-
-	if (slot->format != FT_GLYPH_FORMAT_BITMAP) {
-		fprintf(stderr, "format was not FL_GLYPH_FORMAT_BITMAP");
-		goto cleanup;
-	}
-
-	if (slot->bitmap.pixel_mode != FT_PIXEL_MODE_MONO) {
-		fprintf(stderr, "pixel_mode was not FL_PIXEL_MODE_MONO");
-	}
-
-	render_char(slot, image, 0);
-
-	rows  = slot->bitmap.rows;
-	width = slot->bitmap.width;
 
 	size_t x, y;
-
-	for (y = 0; y < rows; y++) {
+	for (y = 0; y < image_height; y++) {
 		printf("%2zd|", y);
-		for (x = 0; x < width; x++) {
-			datum = image[y][x] ? '*' : ' ';
-			putchar(datum);
+		for (x = 0; x < image_width; x++) {
+			putchar(image[y][x] ? '*' : ' ');
 		}
 		printf("|\n");
 	}
