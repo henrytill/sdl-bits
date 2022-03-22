666f774ee076ba1475c90e53f3f027d1042972d0
Henry Till <henrytill@gmail.com>
2022-03-22 05:49:50 -0700

    rename some constants

--- tool/render_test_bitmap.c
+++ tool/render_test_bitmap.c
@@ -5,7 +5,7 @@ enum {
     HEIGHT_PIXELS = 2,
 };
 
-static const bmp_pixel_ARGB32_t target_buff[] = {
+static const bmp_pixel_ARGB32_t TARGET_BUFF[] = {
     {0xFF, 0x00, 0x00, 0x7F},
     {0x00, 0xFF, 0x00, 0x7F},
     {0x00, 0x00, 0xFF, 0x7F},
@@ -25,7 +25,7 @@ int main(int argc, char *argv[]) {
     (void)argc;
     (void)argv;
 
-    error = bmp_write_bitmap_v4(target_buff, WIDTH_PIXELS, HEIGHT_PIXELS, BMP_FILE);
+    error = bmp_write_bitmap_v4(TARGET_BUFF, WIDTH_PIXELS, HEIGHT_PIXELS, BMP_FILE);
     if (error != 0) {
         goto out;
     }
