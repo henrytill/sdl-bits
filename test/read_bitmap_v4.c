966d5d5483e53478066d892fee2fa51c02d2ee04
Henry Till <henrytill@gmail.com>
2022-03-24 18:16:02 -0700

    add const where appropriate

--- test/read_bitmap_v4.c
+++ test/read_bitmap_v4.c
@@ -4,7 +4,7 @@
 #include "bmp.h"
 #include "test.h"
 
-static char *const BMP_FILE = "./assets/test.bmp";
+static const char *const BMP_FILE = "./assets/test.bmp";
 
 int main(int argc, char *argv[]) {
     int                ret   = 1;
