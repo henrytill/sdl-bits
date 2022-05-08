0f69d961b7515a560f2f68228e99dea2d9b4e12a
Henry Till <henrytill@gmail.com>
2022-05-08 15:55:49 -0700

    main: rework frame delay

--- src/util.h
+++
@@ -1,9 +0,0 @@
-#ifndef SDL_BITS_UTIL_H
-#define SDL_BITS_UTIL_H
-
-#include <stdint.h>
-
-/* Ref: http://locklessinc.com/articles/sat_arithmetic/ */
-uint32_t util_uint32_sat_sub(uint32_t x, uint32_t y);
-
-#endif /* SDL_BITS_UTIL_H */
