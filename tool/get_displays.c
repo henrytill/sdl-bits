82f5f2804d688ca24c14f39182a10b8ce75c362f
Henry Till <henrytill@gmail.com>
2022-05-13 14:45:07 -0700

    get_displays: tweak

--- tool/get_displays.c
+++ tool/get_displays.c
@@ -27,16 +27,14 @@ int main(int argc, char *argv[]) {
         if (error != SUCCESS) {
             const char *error_msg = SDL_GetError();
             SDL_Log("Failed to get display mode for display #%d: %s", i, error_msg);
-            goto out;
-        } else {
-            SDL_Log("Display #%d: display mode is %dx%d @ %d hz.",
-                    i,
-                    display_mode.w,
-                    display_mode.h,
-                    display_mode.refresh_rate);
+            break;
         }
+        SDL_Log("Display #%d: display mode is %dx%d @ %d hz.",
+                i,
+                display_mode.w,
+                display_mode.h,
+                display_mode.refresh_rate);
     }
-
 out:
     SDL_Quit();
     return error;
