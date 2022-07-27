2b48eb2d8d8605a6de39ac744d76df7b077a776f
Henry Till <henrytill@gmail.com>
2022-07-26 22:46:42 -0700

    tool: remove generate_atlas_from_ttf and related

--- assets/generate.py
+++ assets/generate.py
@@ -12,15 +12,6 @@ UCS_FONTS = {
     'output_dir': 'ucs-fonts'
 }
 
-# https://github.com/octaviopardo/EBGaramond12
-EB_GARAMOND_12 = {
-    'target': 'EBGaramond12',
-    'url': 'https://github.com/octaviopardo/EBGaramond12/archive/e608414f52e532b68e2182f96b4ce9db35335593.tar.gz',
-    'tarball': 'EBGaramond12.tar.gz',
-    'output_dir': 'EBGaramond12'
-}
-
-
 def fetch(url, file):
     with urllib.request.urlopen(url) as src:
         with open(file, 'wb') as dst:
@@ -40,22 +31,11 @@ def ucs_fonts():
         expand(UCS_FONTS['tarball'], UCS_FONTS['output_dir'])
 
 
-def eb_garamond_12():
-    if not os.path.isfile(EB_GARAMOND_12['tarball']):
-        print('Downloading ' + EB_GARAMOND_12['url'])
-        fetch(EB_GARAMOND_12['url'], EB_GARAMOND_12['tarball'])
-    if not os.path.isdir(EB_GARAMOND_12['output_dir']):
-        expand(EB_GARAMOND_12['tarball'], EB_GARAMOND_12['output_dir'])
-
-
 def main():
     argv = sys.argv
     print('Running ' + argv[0] + ' with target: ' + argv[1])
     if argv[1] == UCS_FONTS['target']:
         ucs_fonts()
-    elif argv[1] == EB_GARAMOND_12['target']:
-        eb_garamond_12()
-
 
 if __name__ == "__main__":
     main()
