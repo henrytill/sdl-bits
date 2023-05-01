ce4edd68814a496f33b2919937521abbc8fc1183
Henry Till <henrytill@gmail.com>
2023-05-01 15:55:19 -0700

    now that we have static_assert, let's use it

--- src/prelude.h
+++ src/prelude.h
@@ -68,4 +68,15 @@ DEFINE_TRIVIAL_CLEANUP_FUNC(SDL_AudioDeviceID, SDL_CloseAudioDevice);
 DEFINE_TRIVIAL_CLEANUP_FUNC(lua_State *, lua_close);
 #define _cleanup_lua_State_ _cleanup_(lua_closep)
 
+/* Let's not use SDL-defined numeric types */
+
+static_assert(sizeof(Uint16) == sizeof(uint16_t), "SDL-defined Uint16 is not uint16_t");
+static_assert(sizeof(Uint32) == sizeof(uint32_t), "SDL-defined Uint32 is not uint32_t");
+static_assert(sizeof(Uint64) == sizeof(uint64_t), "SDL-defined Uint64 is not uint64_t");
+static_assert(sizeof(Sint16) == sizeof(int16_t), "SDL-defined Sint16 is not int16_t");
+static_assert(sizeof(Sint32) == sizeof(int32_t), "SDL-defined Sint32 is not int32_t");
+static_assert(sizeof(Sint64) == sizeof(int64_t), "SDL-defined Sint64 is not int64_t");
+static_assert(sizeof(SDL_AudioFormat) == sizeof(uint16_t), "SDL-defined SDL_AudioFormat is not uint16_t");
+static_assert(sizeof(SDL_AudioDeviceID) == sizeof(uint32_t), "SDL-defined SDL_AudioDeviceID is not uint32_t");
+
 #endif /* SDL_BITS_PRELUDE_H */
