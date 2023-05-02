5bc888a2a6c078c64af542813fb066942228793e
Henry Till <henrytill@gmail.com>
2023-05-01 22:13:56 -0700

    bmp_read_bitmap{,_v4}: use _cleanup_str_

--- test/bmp_read_bitmap.c
+++ test/bmp_read_bitmap.c
@@ -10,29 +10,27 @@
 #include <stdlib.h>
 
 #include "bmp.h"
-#include "macro.h"
+#include "prelude.h"
 
 static const char *const BMP_FILE = "./assets/sample_24bit.bmp";
 
 int main(_unused_ int argc, _unused_ char *argv[]) {
   extern const char *const BMP_FILE;
 
-  int ret = EXIT_FAILURE;
-  char *image = NULL;
   struct bmp_Filehdr filehdr;
   struct bmp_Infohdr infohdr;
 
+  _cleanup_str_ char *image = NULL;
   if (bmp_read(BMP_FILE, &filehdr, &infohdr, &image) != 0)
-    goto out;
+    return EXIT_FAILURE;
+
   struct bmp_Pixel24 *pixel = (struct bmp_Pixel24 *)image;
   if (pixel->b != 0)
-    goto out;
+    return EXIT_FAILURE;
   if (pixel->g != 0)
-    goto out;
+    return EXIT_FAILURE;
   if (pixel->r != 255)
-    goto out;
-  ret = EXIT_SUCCESS;
-out:
-  free(image);
-  return ret;
+    return EXIT_FAILURE;
+
+  return EXIT_SUCCESS;
 }
--- test/bmp_read_bitmap_v4.c
+++ test/bmp_read_bitmap_v4.c
@@ -10,31 +10,29 @@
 #include <stdlib.h>
 
 #include "bmp.h"
-#include "macro.h"
+#include "prelude.h"
 
 static const char *const BMP_FILE = "./assets/test.bmp";
 
 int main(_unused_ int argc, _unused_ char *argv[]) {
   extern const char *const BMP_FILE;
 
-  int ret = EXIT_FAILURE;
-  char *image = NULL;
   struct bmp_Filehdr filehdr;
   struct bmp_V4hdr v4hdr;
 
+  _cleanup_str_ char *image = NULL;
   if (bmp_v4read(BMP_FILE, &filehdr, &v4hdr, &image) != 0)
-    goto out;
+    return EXIT_FAILURE;
+
   struct bmp_Pixel32 *pixel = (struct bmp_Pixel32 *)image;
   if (pixel->b != 255)
-    goto out;
+    return EXIT_FAILURE;
   if (pixel->g != 0)
-    goto out;
+    return EXIT_FAILURE;
   if (pixel->r != 0)
-    goto out;
+    return EXIT_FAILURE;
   if (pixel->a != 127)
-    goto out;
-  ret = EXIT_SUCCESS;
-out:
-  free(image);
-  return ret;
+    return EXIT_FAILURE;
+
+  return EXIT_SUCCESS;
 }
