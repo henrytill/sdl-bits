33e231c50b213e5efe51e279354ea456326a042f
Henry Till <henrytill@gmail.com>
2023-05-19 01:37:18 -0700

    msgq_{posix,sdl}: tweak

--- lib/msgq/msgq_sdl.c
+++ lib/msgq/msgq_sdl.c
@@ -3,41 +3,41 @@
 #include "msgq.h"
 
 struct msgq_queue {
-  msgq_message *buffer;   // Buffer to hold messages
-  uint32_t capacity; // Maximum size of the buffer
-  size_t front;      // Index of the front message in the buffer
-  size_t rear;       // Index of the rear message in the buffer
-  SDL_sem *empty;    // Semaphore to track empty slots in the buffer
-  SDL_sem *full;     // Semaphore to track filled slots in the buffer
-  SDL_mutex *lock;   // Mutex lock to protect buffer access
+  msgq_message *buffer; // Buffer to hold messages
+  uint32_t capacity;    // Maximum size of the buffer
+  size_t front;         // Index of the front message in the buffer
+  size_t rear;          // Index of the rear message in the buffer
+  SDL_sem *empty;       // Semaphore to track empty slots in the buffer
+  SDL_sem *full;        // Semaphore to track filled slots in the buffer
+  SDL_mutex *lock;      // Mutex lock to protect buffer access
 };
 
-static const char *const MESSAGE_QUEUE_FAILURE_STR[] = {
+static const char *const MSGQ_FAILURE_STR[] = {
 #define X(variant, i, str) [-(MSGQ_FAILURE_##variant)] = (str),
   MSGQ_FAILURE_VARIANTS
 #undef X
 };
 
-static const char *const MESSAGE_TAG_STR[] = {
+static const char *const MSG_TAG_STR[] = {
 #define X(variant, i, str) [MSG_TAG_##variant] = (str),
   MSG_TAG_VARIANTS
 #undef X
 };
 
 const char *msgq_failure(int rc) {
-  extern const char *const MESSAGE_QUEUE_FAILURE_STR[];
+  extern const char *const MSGQ_FAILURE_STR[];
   if (rc > MSGQ_FAILURE_MALLOC || rc < MSGQ_FAILURE_MUTEX_UNLOCK) {
     return NULL;
   }
-  return MESSAGE_QUEUE_FAILURE_STR[-rc];
+  return MSGQ_FAILURE_STR[-rc];
 }
 
 const char *msgq_tag(int tag) {
-  extern const char *const MESSAGE_TAG_STR[];
+  extern const char *const MSG_TAG_STR[];
   if (tag > MSG_TAG_QUIT || tag < MSG_TAG_NONE) {
     return NULL;
   }
-  return MESSAGE_TAG_STR[tag];
+  return MSG_TAG_STR[tag];
 }
 
 static int msgq_init(msgq_queue *queue, uint32_t capacity) {
