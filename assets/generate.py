3c1ea02d92c08716473728330b28a7dc61fe259a
Henry Till <henrytill@gmail.com>
2024-04-17 15:57:04 -0700

    assets/generate.py: reformat

--- assets/generate.py
+++ assets/generate.py
@@ -8,21 +8,21 @@ import sys
 
 # https://www.cl.cam.ac.uk/~mgk25/ucs-fonts.html
 UCS_FONTS = {
-    'target': 'ucs_fonts',
-    'url': 'https://www.cl.cam.ac.uk/~mgk25/download/ucs-fonts.tar.gz',
-    'tarball': 'ucs-fonts.tar.gz',
-    'output_dir': 'ucs-fonts',
+    "target": "ucs_fonts",
+    "url": "https://www.cl.cam.ac.uk/~mgk25/download/ucs-fonts.tar.gz",
+    "tarball": "ucs-fonts.tar.gz",
+    "output_dir": "ucs-fonts",
 }
 
 
 logging.basicConfig(stream=sys.stdout)
-logger = logging.getLogger('generate')
+logger = logging.getLogger("generate")
 logger.setLevel(logging.INFO)
 
 
 def fetch(url, file):
     with urllib.request.urlopen(url) as src:
-        with open(file, 'wb') as dst:
+        with open(file, "wb") as dst:
             shutil.copyfileobj(src, dst)
 
 
@@ -32,19 +32,19 @@ def expand(source, output_dir):
 
 
 def ucs_fonts():
-    if not os.path.isfile(UCS_FONTS['tarball']):
-        logger.info('Downloading %s', UCS_FONTS['url'])
-        fetch(UCS_FONTS['url'], UCS_FONTS['tarball'])
-    if not os.path.isdir(UCS_FONTS['output_dir']):
-        expand(UCS_FONTS['tarball'], UCS_FONTS['output_dir'])
+    if not os.path.isfile(UCS_FONTS["tarball"]):
+        logger.info("Downloading %s", UCS_FONTS["url"])
+        fetch(UCS_FONTS["url"], UCS_FONTS["tarball"])
+    if not os.path.isdir(UCS_FONTS["output_dir"]):
+        expand(UCS_FONTS["tarball"], UCS_FONTS["output_dir"])
 
 
 def main():
     argv = sys.argv
-    logger.debug('Target: %s', argv[1])
-    if argv[1] == UCS_FONTS['target']:
+    logger.debug("Target: %s", argv[1])
+    if argv[1] == UCS_FONTS["target"]:
         ucs_fonts()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
